3c39ad37ff8c704ba70b06d3a9ab1de0d540d27e
Irfan Shah <irfan@pinelabs.com>
2024-07-22 09:38:01 +0530

    use llama3 example

--- main.py
+++ main.py
@@ -1,49 +1,101 @@
-import ollama
+import openai
 import streamlit as st
-import asyncio
-import time
-from openai import AsyncOpenAI
-from token_count import TokenCount
+from streamlit_chat import message
+
+# Setting page title and header
+st.set_page_config(page_title="Open Sesame!", page_icon=":robot_face:")
+st.markdown("<h1 style='text-align: center;'>SETU finbot - Open Sesame! </h1>", unsafe_allow_html=True)
+
+client = openai.Client(base_url="http://localhost:11434/v1", api_key="ignore-me")
+model_name = "llama3"
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = []
+if 'cost' not in st.session_state:
+    st.session_state['cost'] = []
+if 'total_tokens' not in st.session_state:
+    st.session_state['total_tokens'] = []
+if 'total_cost' not in st.session_state:
+    st.session_state['total_cost'] = 0.0
+
+# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+st.sidebar.title("Sidebar")
+counter_placeholder = st.sidebar.empty()
+counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
 
-title = "Some Bank Chatbot"
-st.set_page_config(page_title=title, layout="wide")
-st.title(title)
+# reset everything
+if clear_button:
+    st.session_state['generated'] = []
+    st.session_state['past'] = []
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+    st.session_state['number_tokens'] = []
+    st.session_state['model_name'] = []
+    st.session_state['cost'] = []
+    st.session_state['total_cost'] = 0.0
+    st.session_state['total_tokens'] = []
+    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
 
-generate = st.button("Generate", type="primary")
 
-model = "llama3"
-prompt = "Can a python eat a lion?"
-
-col = st.columns(1)
-col.append(f"# :blue[{model}]")
-
-meta = None
-
-body = col[0].empty()
-client = AsyncOpenAI(base_url="http://localhost:11434/v1", api_key="ignore-me")
-
-async def run_prompt(placeholder, meta, prompt, model):
-    stream = await client.chat.completions.create( 
-        model=model,
-        messages=[{"role": "system", "content": "You are a helpful assistant."},
-                  {"role": "user", "content": prompt},],
-        stream=True
+# generate a response
+def generate_response(prompt):
+    st.session_state['messages'].append({"role": "user", "content": prompt})
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=st.session_state['messages']
     )
-    streamed_text = ""
-    async for chunk in stream:
-        chunk_content = chunk.choices[0].delta.content
-        if chunk_content is not None:
-            streamed_text = streamed_text + chunk_content 
-            placeholder.write(streamed_text) 
+    response_content = response.choices[0].message.content
+    st.session_state['messages'].append({"role": "assistant", "content": response_content})
 
-async def main():
-    await asyncio.gather(
-        run_prompt(body, meta, prompt=prompt, model=model)
-    )
+    # print(st.session_state['messages'])
+    total_tokens = response.usage.total_tokens
+    prompt_tokens = response.usage.prompt_tokens
+    completion_tokens = response.usage.completion_tokens
+    return response_content, total_tokens, prompt_tokens, completion_tokens
 
-if generate:
-    if prompt == "":
-        st.warning("Please enter a prompt")
-    else:
-        asyncio.run(main())
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+        st.session_state['model_name'].append(model_name)
+        st.session_state['total_tokens'].append(total_tokens)
+
+        # from https://openai.com/pricing#language-models
+        if model_name == "GPT-3.5":
+            cost = total_tokens * 0.002 / 1000
+        else:
+            cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+
+        st.session_state['cost'].append(cost)
+        st.session_state['total_cost'] += cost
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
+            st.write(
+                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
+            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
